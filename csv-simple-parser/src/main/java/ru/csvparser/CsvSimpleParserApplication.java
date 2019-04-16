de4f08ed3ff25b32972aad7b267029e5930ff426
vasiliy <vasiliy@DESKTOP-RN50IIN>
2019-04-16 22:07:23 +0300

    start

---
+++ csv-simple-parser/src/main/java/ru/csvparser/CsvSimpleParserApplication.java
@@ -0,0 +1,13 @@
+package ru.csvparser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsvSimpleParserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsvSimpleParserApplication.class, args);
+	}
+
+}
