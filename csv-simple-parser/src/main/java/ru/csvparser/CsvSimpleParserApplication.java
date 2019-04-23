7da3749438566368891d6e0b3d689564222ed4e0
platonovv <platonovv@DESKTOP-2FJA6A6>
2019-04-23 18:40:38 +0300

    prepare for Tomcat change referenses in html...

--- csv-simple-parser/src/main/java/ru/csvparser/CsvSimpleParserApplication.java
+++ csv-simple-parser/src/main/java/ru/csvparser/CsvSimpleParserApplication.java
@@ -2,12 +2,19 @@ package ru.csvparser;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CsvSimpleParserApplication {
+public class CsvSimpleParserApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CsvSimpleParserApplication.class, args);
 	}
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(CsvSimpleParserApplication.class);
+    }
 
 }
